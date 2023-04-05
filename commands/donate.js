9ca36dd506904c53947b3166049d6fc57a35b936
Dolix7 <51421090+Dolix7@users.noreply.github.com>
2023-04-05 16:46:18 +0200

    add donate command

---
+++ commands/donate.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    // Set the name and description of the command
+    .setName('donate')
+    .setDescription(
+      'Vous pouvez soutenir le créateur du bot en lui payant un café !'
+    ),
+  async execute(interaction) {
+    // Replu the link to the buy me a coffee page
+    await interaction.reply({
+      content:
+        'Pour faire un don, ça se passe ici : https://www.buymeacoffee.com/vrh9ft859hx Merci Beaucoup !',
+      ephemeral: true,
+    });
+  },
+};
